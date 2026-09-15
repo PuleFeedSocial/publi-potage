54122f25e2a5810b660240f0c54703ee9b963b21
Victor <victoraramirezp@gmail.com>
2026-09-14 21:42:13 -0300

    fix: colision de variable global zonasData en sucursales.html rompia todo el script de la pagina - renombrada a sucursalesZonasData

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'potage-v8';
+const CACHE = 'potage-v9';
 const STATIC = [
   '/dashboard.html', '/index.html', '/login.html', '/register.html',
   '/grupos.html', '/informes.html', '/logs.html', '/account-settings.html',
