7ba12db1ca9329a54a4bcba618e209267060ef8d
Victor <victoraramirezp@gmail.com>
2026-09-15 04:24:05 -0300

    feat: buscador de direcciones con auto-sugerencias y radio de 5km separado del buscador general de sucursales

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'potage-v12';
+const CACHE = 'potage-v13';
 const STATIC = [
   '/dashboard.html', '/index.html', '/login.html', '/register.html',
   '/grupos.html', '/informes.html', '/logs.html', '/account-settings.html',
