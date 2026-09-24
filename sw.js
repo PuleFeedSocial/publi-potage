c2d33ffc2ca212e08e40f7ce3076b625d12c3488
Victor <victoraramirezp@gmail.com>
2026-09-24 11:59:55 -0300

    feat: marca de agua con logo 'A' y nombre 'Altas Engeniering' en sidebar, login, register e index (SW v28)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'potage-v27';
+const CACHE = 'potage-v28';
 const STATIC = [
   '/dashboard.html', '/index.html', '/login.html', '/register.html',
   '/grupos.html', '/informes.html', '/logs.html', '/account-settings.html',
